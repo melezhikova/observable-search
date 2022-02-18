7417c8e3ab1deb83c86a76839dd2d0bd098ef1e3
Liudmila Melezhikova <melezhikova@yandex.ru>
2022-02-18 14:59:18 +0500

    second commit

--- src/Skills.js
+++ src/Skills.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { changeSearchField } from './actions/actionCreators';
-import store from './store';
 
 export default function Skills() {
     const { items, loading, error, search } = useSelector(state => state.skills);
